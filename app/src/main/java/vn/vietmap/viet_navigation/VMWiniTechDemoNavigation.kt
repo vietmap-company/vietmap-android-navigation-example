1ea812d91b52cd8975ab915e6da9ce800852eef9
thanhdang198 <dangtrongthanh1998@gmail.com>
2023-10-24 11:08:02 +0700

    add map style url

--- app/src/main/java/vn/vietmap/viet_navigation/VMWiniTechDemoNavigation.kt
+++ app/src/main/java/vn/vietmap/viet_navigation/VMWiniTechDemoNavigation.kt
@@ -343,7 +343,7 @@ class VMWiniTechDemoNavigation : AppCompatActivity(), OnMapReadyCallback, Progre
         vietmapGL = p0
         vietmapGL!!.setStyle(
             Style.Builder()
-                .fromUri("https://maps.vietmap.vn/api/navigations/tilemaps/styles/light?apikey=89cb1c3c260c27ea71a115ece3c8d7cec462e7a4c14f0944")
+                .fromUri("https://maps.vietmap.vn/api/maps/light/styles.json?apikey=YOUR_API_KEY_HERE")
         ) { style: Style? ->
             initLocationEngine()
 
